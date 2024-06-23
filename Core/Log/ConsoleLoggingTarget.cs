48717c450d49156e816986fad4d7b89f9988295f
winemp83Main <koehne83@googlemail.com>
2024-06-23 21:58:08 +0200

    Add First CommitProjektdateien hinzufügen.

---
+++ Core/Log/ConsoleLoggingTarget.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Core.Log
+{
+    public class ConsoleLoggingTarget
+    {
+        public void LogMessage(string message)
+        {
+            Log($"[{DateTime.Now:HH:mm:ss}] {message}");
+        }
+
+        public void LogError(string errorMessage, string functionName, int lineNumber)
+        {
+            Log($"[{DateTime.Now:HH:mm:ss}] ERROR: {errorMessage} in {functionName}, line {lineNumber}");
+        }
+
+        private void Log(string logMessage)
+        {
+            Console.WriteLine(logMessage);
+        }
+    }
+}
---
+++ Core/Log/FileLoggingTarget.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+
+namespace Core.Log
+{
+    public class FileLoggingTarget
+    {
+        private readonly string logFilePath;
+        private readonly string logFileName;
+        private readonly string fullPath;
+
+        public FileLoggingTarget(string fileName)
+        {
+            logFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Logs");
+            logFileName = fileName;
+            if (!Directory.Exists(logFilePath))
+            {
+                Directory.CreateDirectory(logFilePath);
+            }
+
+            fullPath = Path.Combine(logFilePath, logFileName);
+        }
+
+        public void LogMessage(string message)
+        {
+            Log($"[{DateTime.Now:HH:mm:ss}] {message}");
+        }
+
+        public void LogError(string errorMessage, string functionName, int lineNumber)
+        {
+            Log($"[{DateTime.Now:HH:mm:ss}] ERROR: {errorMessage} in {functionName}, line {lineNumber}");
+        }
+
+        private void Log(string logMessage)
+        {
+            try
+            {
+                if (!Directory.Exists(logFilePath))
+                {
+                    Directory.CreateDirectory(logFilePath);
+                }
+                using StreamWriter writer = new StreamWriter(fullPath, true);
+                writer.WriteLine(logMessage);
+                writer.Flush();
+                writer.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error logging to file: {ex.Message}");
+            }
+        }
+    }
+}
